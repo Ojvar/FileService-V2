a3f75eebd489527d8343859772e4488f0f992b2b
Ojvar <MOjvar.AmirHossein@Gmail.com>
2022-09-30 11:45:03 +0330

    fix: file-manager-service.dto

---
+++ src/dto/file-manager.service.dto.ts
@@ -0,0 +1,55 @@
+import {model, property} from '@loopback/repository';
+import {ExpireTime} from '../types';
+
+export namespace FILE_MANAGER_SERVICE {
+  @model({jsonSchema: {description: 'Allowed file item'}})
+  export class AllowedFile {
+    @property({
+      type: 'string',
+      required: true,
+      jsonSchema: {description: 'Field name', minLength: 1, maxLength: 50},
+    })
+    field: string;
+
+    @property({
+      type: 'number',
+      required: true,
+      jsonSchema: {description: 'Maximum file size in bytes', minimum: 1},
+    })
+    max_size: number;
+
+    @property({
+      type: 'string',
+      required: false,
+      jsonSchema: {description: 'Mime type'},
+    })
+    mime_type?: string | null;
+  }
+  export type AllowedFiles = AllowedFile[];
+
+  @model({jsonSchema: {description: 'Get token request'}})
+  export class GetTokenRequestDTO {
+    @property.array(AllowedFile, {require: true}) allowed_files: AllowedFiles;
+    @property({type: 'date', required: false}) expire_at: ExpireTime;
+    @property({
+      type: 'string',
+      requird: true,
+      mongodb: {dataType: 'ObjectId'},
+      jsonSchema: {description: 'Allowed user id'},
+    })
+    allowed_user: string;
+  }
+
+  @model({jsonSchema: {description: 'Get token response'}})
+  export class GetTokenResponseDTO {
+    @property({
+      type: 'string',
+      mongodb: {dataType: 'ObjectId'},
+      jsonSchema: {description: 'Token id'},
+    })
+    id: string;
+
+    @property({type: 'date', jsonSchema: {description: 'Expire time'}})
+    expire_at?: ExpireTime;
+  }
+}
