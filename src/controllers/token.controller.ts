ee0c99b43a3ca0b98752930e2f5048cafcfae8d6
Ojvar <MOjvar.AmirHossein@Gmail.com>
2022-10-07 13:21:48 +0330

    add: Get credential data

--- src/controllers/token.controller.ts
+++ src/controllers/token.controller.ts
@@ -1,6 +1,7 @@
 import {inject} from '@loopback/core';
 import {
   del,
+  get,
   getModelSchemaRef,
   param,
   patch,
@@ -8,14 +9,34 @@ import {
   requestBody,
 } from '@loopback/rest';
 import {FILE_MANAGER_SERVICE_DTO} from '../dto';
+import {Credential} from '../models';
 import {FileManagerService, FILE_MANAGER_SERVICE} from '../services';
 
 export class TokenController {
   /* TODO: CHECK USER JWT - FOR AUTHORIZATION */
+  @get('/token/{token}/{user_id}', {
+    description: 'Get credential data',
+    summary: 'Get credential data',
+    tags: ['credential'],
+    responses: {
+      200: {
+        description: 'Credential Data',
+        content: {'application/json': getModelSchemaRef(Credential)},
+      },
+    },
+  })
+  async getFilesList(
+    @param.path.string('token') token: string,
+    @param.path.string('user_id') userId: string,
+  ): Promise<Credential> {
+    return this.fileManagerService.getCredential(token, userId);
+  }
+
+  /* TODO: CHECK USER JWT - FOR AUTHORIZATION */
   @del('/token/{token}/{user_id}', {
     description: 'Reject a certificate',
     summary: 'Reject a certificate',
-    tags: ['token'],
+    tags: ['credential'],
     responses: {204: {description: 'Reject successfully'}},
   })
   async rejectCertificate(
@@ -29,7 +50,7 @@ export class TokenController {
   @patch('/token/{token}/{user_id}', {
     description: 'Commit a certificate',
     summary: 'Commit a certificate',
-    tags: ['token'],
+    tags: ['credential'],
     responses: {204: {description: 'Commit successfully'}},
   })
   async commitCertificate(
@@ -43,7 +64,7 @@ export class TokenController {
   @post('/token', {
     description: 'Generate a file upload certificate and return file-token',
     summary: 'Generate a new file-upload certificate',
-    tags: ['token'],
+    tags: ['credential'],
     responses: {
       200: {
         description: 'Genereated token data',
@@ -57,7 +78,7 @@ export class TokenController {
       },
     },
   })
-  async getToken(
+  async generateToken(
     @requestBody() body: FILE_MANAGER_SERVICE_DTO.GetTokenRequestDTO,
   ): Promise<unknown> {
     return this.fileManagerService.getToken(body);
