72be337ff4bb0136f71a86ed042c93d6d6239c6c
Ojvar <MOjvar.AmirHossein@gmail.com>
2023-01-23 13:07:39 +0330

    fix bug of FileDownload.Controller

--- src/controllers/file-downloader.controller.ts
+++ src/controllers/file-downloader.controller.ts
@@ -1,15 +1,10 @@
-import { inject, intercept } from '@loopback/core';
+import { inject } from '@loopback/core';
 import { repository } from '@loopback/repository';
 import { get, param, Response, RestBindings } from '@loopback/rest';
 import { resolve } from 'path';
 import { OBJECT_ID_PATTERN } from '../dto';
 import { STORAGE_DIRECTORY } from '../interceptors';
-import { SECURITY_ROLES } from '../keys';
-import {
-  KeycloakSecurity,
-  KEYCLOAK_SECURITY_SERVICE,
-  protect,
-} from '../lib-keycloak/src';
+import { KeycloakSecurity, KEYCLOAK_SECURITY_SERVICE } from '../lib-keycloak/src';
 import { FileRepository } from '../repositories';
 import { FileManagerService, FILE_MANAGER_SERVICE } from '../services';
 
@@ -19,11 +14,8 @@ export class FileDownloaderController {
     @inject(FILE_MANAGER_SERVICE)
     private fileManagerService: FileManagerService,
     @repository(FileRepository) private fileRepository: FileRepository,
-    @inject(KEYCLOAK_SECURITY_SERVICE)
-    private keycloakSecurityService: KeycloakSecurity,
   ) { }
 
-  @intercept(protect(SECURITY_ROLES.FILE_SERVICE_MANAGER))
   @get('/files/download/{id}', {
     tags: ['files'],
     description: 'Download file by file-id',
@@ -50,11 +42,9 @@ export class FileDownloaderController {
     accessToken = '',
   ): Promise<Response> {
     const fileInfo = await this.fileRepository.getFileInfo(id);
-    const { sub: userId } = await this.keycloakSecurityService.getUserInfo();
-    if (fileInfo.owner !== userId || fileInfo.is_private) {
+    if (fileInfo.is_private) {
       await this.fileManagerService.checkAccessToken(id, accessToken);
     }
-
     const filepath = resolve(this.storageDirectory, fileInfo.getId());
     response.setHeader('Content-type', fileInfo.mime);
     response.download(filepath, fileInfo.original_name);
