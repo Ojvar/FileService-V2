21a2f5190160e193d8e51b84cbd70c03bc201b9b
Ojvar <MOjvar.AmirHossein@gmail.com>
2023-01-26 14:10:51 +0330

    fix upload-file.controller

--- src/controllers/file-uploader.controller.ts
+++ src/controllers/file-uploader.controller.ts
@@ -8,7 +8,11 @@ import {
 } from '@loopback/rest';
 import { FileHandlerInterceptor } from '../interceptors';
 import { SECURITY_ROLES } from '../keys';
-import { protect } from '../lib-keycloak/src';
+import {
+  KeycloakSecurity,
+  KEYCLOAK_SECURITY_SERVICE,
+  protect,
+} from '../lib-keycloak/src';
 import { UploadedFile } from '../models';
 import { FileManagerService, FILE_MANAGER_SERVICE } from '../services';
 
@@ -16,11 +20,13 @@ export class FileUploaderController {
   constructor(
     @inject(FILE_MANAGER_SERVICE)
     private fileManagerService: FileManagerService,
+    @inject(KEYCLOAK_SECURITY_SERVICE)
+    private keycloakSecurity: KeycloakSecurity,
   ) { }
 
-  @intercept(protect(SECURITY_ROLES.FILE_SERVICE_MANAGER))
+  @intercept(protect(SECURITY_ROLES.NO_BODY))
   @intercept(FileHandlerInterceptor.BINDING_KEY)
-  @post('/files/{user_id}/{field}', {
+  @post('/files/{field}', {
     tags: ['files'],
     description: 'Upload files',
     summary: 'Upload files',
@@ -36,7 +42,6 @@ export class FileUploaderController {
   async fileUpload(
     @param.header.string('file-token', { description: 'FileUpload token' })
     token: string,
-    @param.path.string('user_id', { description: "User's id" }) userId: string,
     @param.path.string('field', { description: 'Field name' })
     field: string,
     @requestBody.file({
@@ -45,6 +50,7 @@ export class FileUploaderController {
     })
     request: Request,
   ): Promise<UploadedFile> {
+    const { sub: userId } = await this.keycloakSecurity.getUserInfo();
     return this.fileManagerService.uploadFile(userId, token, field, request);
   }
 }
